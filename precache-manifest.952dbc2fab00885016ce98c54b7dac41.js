7e1d840372783a4b19d34b086e0b9e04fecf5d13
David Pomerenke <david.pomerenke@uni-konstanz.de>
2019-11-13 13:57:09 +0000

    Updates

---
+++ precache-manifest.952dbc2fab00885016ce98c54b7dac41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e068f6aca4f408533bcc7b43ecb83db",
+    "url": "/checkers/index.html"
+  },
+  {
+    "revision": "8a8786977557f390bfd6",
+    "url": "/checkers/static/css/main.2b210f9a.chunk.css"
+  },
+  {
+    "revision": "fc29690b05cb646ee231",
+    "url": "/checkers/static/js/2.8ab87316.chunk.js"
+  },
+  {
+    "revision": "8a8786977557f390bfd6",
+    "url": "/checkers/static/js/main.13f2b85c.chunk.js"
+  },
+  {
+    "revision": "db4cc610b428fbf56daf",
+    "url": "/checkers/static/js/runtime-main.c1cbcd3e.js"
+  }
+]);
