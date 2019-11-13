fea46a4f5e62ee965282fe805be2929b97edfc9b
David Pomerenke <david.pomerenke@uni-konstanz.de>
2019-11-13 12:41:48 +0000

    add nice board

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
+
+class Tile extends React.Component {
+  colour (y, x) {
+    return (y + x) % 2 === 0 ? 'light-tile' : 'dark-tile'
+  }
+
+  render () {
+    return (
+      <div className={this.colour(this.props.y, this.props.x)} />
+    )
+  }
+}
+
+class Row extends React.Component {
+  render () {
+    return (
+      <div className='row'>
+        {
+          [0, 1, 2, 3, 4, 5, 6, 7].map(x =>
+            <Tile x={x} y={this.props.y} state={this.props.state} key={x.toString()} />
+          )
+        }
+      </div>
+    )
+  }
+}
+
+class Board extends React.Component {
+  render () {
+    return (
+      <div className='board'>
+        {
+          [0, 1, 2, 3, 4, 5, 6, 7].map(y =>
+            <Row y={y} state={this.props.state} key={y.toString()} />
+          )
+        }
+      </div>
+    )
+  }
+}
 
 function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Board state='' />
   )
 }
 
